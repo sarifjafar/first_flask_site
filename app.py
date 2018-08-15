b3ae83f3ddec6f8ff531c3bde0e741121a181b65
Jafar Sarif <jeffstone1994@gmail.com>
2018-08-15 23:20:53 +0530

    Second Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template
+
+host = '127.0.0.1'
+port = 8000
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+
+
+if __name__ == '__main__':
+    app.run(host=host,port=port,debug=True)
+
